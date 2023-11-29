dcbd0e2742455388d86e5cb22110d944c5171dff
loutix <62959342+loutix@users.noreply.github.com>
2023-11-29 17:09:52 +0100

    Add to VCS from the shortcut Ctrl+Alt+A

---
+++ src/main/java/com/ocrooms/safetynet/controller/FireStationController.java
@@ -0,0 +1,16 @@
+package com.ocrooms.safetynet.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "firestation")
+public class FireStationController {
+
+//    todo
+//     - ajout d'un mapping caserne/adresse ;
+//     - mettre à jour le numéro de la caserne de pompiers d'une adresse ;
+//     - supprimer le mapping d'une caserne ou d'une adresse.
+
+
+}
