83b11759f8f5051e15f45e64c0449e0fe9aa64fd
loutix <62959342+loutix@users.noreply.github.com>
2024-01-18 15:57:39 +0100

    Changes by frank

--- src/main/java/com/ocrooms/safetynet/entities/Firestation.java
+++ src/main/java/com/ocrooms/safetynet/entities/Firestation.java
@@ -1,22 +1,35 @@
 package com.ocrooms.safetynet.entities;
 
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.Range;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Firestation {
 
     @NotNull
     @Length(min = 1, max = 20)
     private String address;
-    private int station;
 
     @NotNull
     @Range(min = 1, max = 20)
     private Integer station;
 
-    public Firestation(String address, int station) {
-        this.address = address;
-        this.station = station;
-    }
+    @Data
+    @AllArgsConstructor
+    public static class FirestationUpdateRequest {
+        @NotNull
+        @Range(min = 1, max = 20, message = "Old station number should be at minimum 1")
+        private Integer oldStation;
 
-    public void trimProperties() {
-        this.address = this.address.trim();
+        @NotNull
+        @Range(min = 1, max = 20, message = "New station number should be at minimum 1")
+        private Integer newStation;
     }
 }
